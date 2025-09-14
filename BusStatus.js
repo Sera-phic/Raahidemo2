445c6ff793300a1ee24f374da9e6534cdd70c438
Khushi Dwivedi <165365625+Sera-phic@users.noreply.github.com>
2025-09-14 14:37:18 +0530

    Create BusStatus.js

---
+++ BusStatus.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function BusStatus() {
+  return (
+    <div className="panel">
+      <h2>🚍 Live Bus Status</h2>
+      <p>Bus #101 → Arriving in 5 min</p>
+      <p>Bus #205 → Arriving in 12 min</p>
+      <p>Bus #330 → Arriving in 18 min</p>
+    </div>
+  );
+}
+
+export default BusStatus;
