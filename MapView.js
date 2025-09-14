2dc1f0ba8a6feae37c639c27a05080eee10221e7
Khushi Dwivedi <165365625+Sera-phic@users.noreply.github.com>
2025-09-14 14:36:24 +0530

    Create MapView.js

---
+++ MapView.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MapView() {
+  return (
+    <div className="map">
+      <h2>🗺️ Map Placeholder</h2>
+      {/* Later you can add Leaflet/Google Maps */}
+    </div>
+  );
+}
+
+export default MapView;
